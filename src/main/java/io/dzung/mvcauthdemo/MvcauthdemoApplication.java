4314fb303cc04789a426d0c08a0affe8c7f8e541
Dzung Nguyen <dzung_min@yahoo.com>
2026-05-22 11:59:38 +0700

    initial commit

---
+++ src/main/java/io/dzung/mvcauthdemo/MvcauthdemoApplication.java
@@ -0,0 +1,13 @@
+package io.dzung.mvcauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcauthdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcauthdemoApplication.class, args);
+	}
+
+}
