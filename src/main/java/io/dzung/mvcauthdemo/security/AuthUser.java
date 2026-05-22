3415b423c5283a629563239a291ff6b1c9ecf1ca
Dzung Nguyen <dzung_min@yahoo.com>
2026-05-22 15:02:34 +0700

    feat: add AuthUser class
    Refs #1

---
+++ src/main/java/io/dzung/mvcauthdemo/security/AuthUser.java
@@ -0,0 +1,34 @@
+package io.dzung.mvcauthdemo.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.dzung.mvcauthdemo.model.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class AuthUser implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
